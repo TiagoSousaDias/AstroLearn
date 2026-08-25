8a33089e21ad425c0dc43633618f50417ee4feed
Tiago Dias <tiago024@gmail.com>
2026-08-26 00:09:02 +0100

    Finish Tut

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-      site: "https://6a8e113c6280b5000859ecff--astrolearntest.netlify.app/"
-
+  site: "https://6a8e113c6280b5000859ecff--astrolearntest.netlify.app/",
+  integrations: [preact()]
 });
