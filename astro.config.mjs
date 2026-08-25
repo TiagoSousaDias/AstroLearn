97769dfcde176a84ffd0665a93efbf5040aea928
Tiago Dias <tiago024@gmail.com>
2026-08-25 23:09:43 +0100

    Added Preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-      site: "https://example.com"
+      site: "https://6a8e113c6280b5000859ecff--astrolearntest.netlify.app/"
 
 });
